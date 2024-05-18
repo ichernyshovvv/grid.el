ab8a8e37e568f36750856f78a23f5159644a2ab4
Ilya Chernyshov <ichernyshovvv@gmail.com>
2024-05-19 00:54:11 +0700

    Update README.org

--- grid-examples.el
+++ grid-examples.el
@@ -1,5 +1,14 @@
 ;; -*- lexical-binding: t; -*-
 
+(defvar grid-calendar
+  "      May 2024      
+Su Mo Tu We Th Fr Sa
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29 30 31   ")
+
 (defvar grid-multiline-str "line 1
 line 2
 line 3")
